5ff20633238cb81c82c3dc1b7c5ef5d92eb616e0
jahabe <janechoius03@gmail.com>
2026-07-11 14:46:48 -0700

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-tmux kill-server || true
 cd ~/mlh-portfolio
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-tmux new-session -d -s flask-server "cd ~/mlh-portfolio && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0"
+systemctl restart myportfolio
