02305d5eaee06446d65943753f329670e4485b94
jahabe <janechoius03@gmail.com>
2026-07-24 15:08:42 -0700

    update redeploy-site.sh to use docker compose

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,6 +2,5 @@
 
 cd ~/mlh-portfolio
 git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
